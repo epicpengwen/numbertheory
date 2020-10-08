86e37c7b351d99a65e07f358fcc4631ff6031b5e
epicpengwen <67085076+epicpengwen@users.noreply.github.com>
2020-10-08 15:03:14 -0400

    Create primitvepythagoreantriples.py

---
+++ primitvepythagoreantriples.py
@@ -0,0 +1,12 @@
+#take users input
+in = raw_input("Enter range: ")
+def primitivetriples(rge):
+    for i in range(1, rge, 2):
+        t = i
+        for j in range(t+2, rge, 2):
+            s = j
+            a = s * t
+            b = (s**2 - t**2) / 2
+            c = (s**2 + t**2) / 2
+            print(a, b, c)
+primitivetriples(in)
