933339dc827e4448459afe00d3bfca3cfd640d06
gkpotter <gkp@bu.edu>
2021-01-18 11:09:26 -0500

    Refactored string manipulation of decoded message.

--- decryption3.py
+++ decryption3.py
@@ -12,20 +12,21 @@ def decrypt(m, k, pikachu):
     RSA decryption taking p, q, and k values to decode a list of numbers and return a message
     """
     #create empty string for list of digits to be added to
-    Answer = ''
+    Answer = []
     message = ''
 
     #iterate through list of numbers and apply chapter 17 methods to solve congruences 
     for number in pikachu:
-        Answer += str(kroots(k, number, m))
+        Answer.append(kroots(k, number, m))
 
     #create dict of letters associated with 2 digit numbers
     #create for loop for this?
     ash = {11:'A', 12:'B', 13:'C', 14:'D', 15:'E', 16:'F', 17:'G', 18:'H', 19:'I', 20:'J', 21:'K', 22:'L', 23:'M', 24:'N', 25:'O', 26:'P', 27:'Q', 28:'R', 29:'S', 30:'T', 31:'U', 32:'V', 33:'W', 34:'X', 35:'Y', 36:'Z'}
 
     #iterate through string of digits by twos to convert into legible message
-    for i in range(0, len(Answer), 2):
-        message += ash[int(Answer[i:i+2])]
+    for code in Answer:
+        if code in ash:
+            message += ash[code]
 
     return(message)
 
@@ -49,7 +50,7 @@ def kroots(k , b , m):
     return(successivesquaring(b, u, m))
     
 if __name__ == '__main__':
-    userlist = [int(item) for item in input("Enter the list of numbers : ").split(", ")]
+    userlist = [int(item.strip(" ")) for item in input("Enter the list of numbers : ").split(",")]
     m = int(input("Enter your value for m: "))
     k = int(input("Enter your value for k: "))
 
